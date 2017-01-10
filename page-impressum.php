59d7e3fcefdf47f7913a0e036e9a30ee8d1016d1
Maik Schmaddebeck <smadback@gmail.com>
2017-01-10 21:18:19 +0100

    Impressum erstellt

---
+++ page-impressum.php
@@ -0,0 +1,29 @@
+<?php
+/*
+  Template Name: Impressum
+*/
+?>
+
+<?php get_header(); ?>
+
+<main>
+  <div class="container">
+    <div class="section center">
+      <h4>Impressum</h4>
+      <div class="divider"></div>
+      <p>
+          Garntante<br>
+          Hammer Steindamm 119 <br>
+          20535 Hamburg <br> <br>
+
+          Tel: 60 43 3500<br>
+          Fax: 60 43 3542<br>
+          E-Mail: <a href="mailto:kontakt@garntante.de">kontakt@garntante.de</a> <br><br>
+
+          Inhaber: Andreas Schmaddebeck <br>
+      </p>
+    </div>
+  </div>
+</main>
+
+<?php get_footer(); ?>
