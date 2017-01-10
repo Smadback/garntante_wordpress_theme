0d43986259d339952b07811eafd56dcd7c189a05
Maik Schmaddebeck <smadback@gmail.com>
2017-01-10 01:15:04 +0100

    Termine fix

--- page-termine-und-kurse.php
+++ page-termine-und-kurse.php
@@ -20,24 +20,24 @@
                   <h4 class="center">Januar '17</h4>
                   <ul class="collapsible popout" data-collapsible="accordion">
                     <li>
-                      <div class="collapsible-header"><i class="material-icons">group</i>Stricktreff am Dienstag</div>
+                      <div class="collapsible-header"><i class="material-icons">group</i>Stricktreff am Dienstag (03.01.)</div>
                       <div class="collapsible-body"><p>
                       Stricken alleine langweilt euch? Dann kommt doch zu uns in die Garntante. <br>
                       Bei einer Tasse Kaffe treffen wir uns jeden <b>1. und 3. Dienstag</b> im Monat von 17:00 bis 19:00 Uhr um in lockerer Runde gemeinsam die
                       Nadeln klappern zu lassen.<br><br>
                       <b>Dauer: </b>17:00 - 19:00 Uhr<br>
-                      <b>Termin: </b>' . $datum . '<br>
+                      <b>Termin: </b>03.01.2017<br>
                       </p>
                     </div>
                   </li>
                     <li>
-                      <div class="collapsible-header"><i class="material-icons">group</i>Stricktreff am Dienstag</div>
+                      <div class="collapsible-header"><i class="material-icons">group</i>Stricktreff am Dienstag (31.01.)</div>
                       <div class="collapsible-body"><p>
                       Stricken alleine langweilt euch? Dann kommt doch zu uns in die Garntante. <br>
                       Bei einer Tasse Kaffe treffen wir uns jeden <b>1. und 3. Dienstag</b> im Monat von 17:00 bis 19:00 Uhr um in lockerer Runde gemeinsam die
                       Nadeln klappern zu lassen.<br><br>
                       <b>Dauer: </b>17:00 - 19:00 Uhr<br>
-                      <b>Termin: </b>' . $datum . '<br>
+                      <b>Termin: </b>31.01.2017<br>
                       </p>
                     </div>
                   </li>
@@ -46,50 +46,96 @@
                   <h4 class="center">Februar '17</h4>
                   <ul class="collapsible popout" data-collapsible="accordion">
                     <li>
-                      <div class="collapsible-header"><i class="material-icons">group</i>Stricktreff am Dienstag</div>
-                      <div class="collapsible-body"><p>
-                      Stricken alleine langweilt euch? Dann kommt doch zu uns in die Garntante. <br>
-                      Bei einer Tasse Kaffe treffen wir uns jeden <b>1. und 3. Dienstag</b> im Monat von 17:00 bis 19:00 Uhr um in lockerer Runde gemeinsam die
-                      Nadeln klappern zu lassen.<br><br>
-                      <b>Dauer: </b>17:00 - 19:00 Uhr<br>
-                      <b>Termin: </b>' . $datum . '<br>
-                      </p>
-                    </div>
-                  </li>
+                      <div class="collapsible-header"><i class="material-icons">group</i>Stricktreff am Dienstag (07.02.)</div>
+                        <div class="collapsible-body"><p>
+                        Stricken alleine langweilt euch? Dann kommt doch zu uns in die Garntante. <br>
+                        Bei einer Tasse Kaffe treffen wir uns jeden <b>1. und 3. Dienstag</b> im Monat von 17:00 bis 19:00 Uhr um in lockerer Runde gemeinsam die
+                        Nadeln klappern zu lassen.<br><br>
+                        <b>Dauer: </b>17:00 - 19:00 Uhr<br>
+                        <b>Termin: </b>07.02.2017<br>
+                        </p>
+                      </div>
+                    </li>
+
                     <li>
-                      <div class="collapsible-header"><i class="material-icons">group</i>Stricktreff am Dienstag</div>
-                      <div class="collapsible-body"><p>
-                      Stricken alleine langweilt euch? Dann kommt doch zu uns in die Garntante. <br>
-                      Bei einer Tasse Kaffe treffen wir uns jeden <b>1. und 3. Dienstag</b> im Monat von 17:00 bis 19:00 Uhr um in lockerer Runde gemeinsam die
-                      Nadeln klappern zu lassen.<br><br>
-                      <b>Dauer: </b>17:00 - 19:00 Uhr<br>
-                      <b>Termin: </b>' . $datum . '<br>
-                      </p>
-                    </div>
-                  </li>
+                      <div class="collapsible-header"><i class="material-icons">event_note</i>Strick-Anfängerkurs (03.02. + 10.02.)</div>
+                        <div class="collapsible-body"><p>
+                        Wenn Sie noch nie gestrickt haben oder sich nur vage an rechte und linke Maschen erinnern, können
+                        Sie sich dies bei uns im Laden beibringen lassen. Wir stricken einen Schal und lernen dabei spielend
+                        z.B. Maschen aufnehmen und zu- oder abnehmen. Kommen Sie dafür zu unserem Strick-Anfängerkurs. <br><br>
+                        <b>Dauer: </b>4 Stunden (jeweils 2 Stunden pro Termin)<br>
+                        <b>Termine: </b>03.02. und 10.02. von 17 bis 19 Uhr<br>
+                        <b>Teilnahmegeb&uuml;hr: </b>40€ pro Person<br>
+                        <b>Material: </b>Ben&ouml;tigt werden 100g Sockenwolle f&uuml;r Nadelst&auml;rke 3,5mm und eine Rundstricknadel in 3,5mm. Das Material kann vor Ort erworben oder von zu Hause mitgebracht werden.
+                        <br><br>
+                       Bitte melden Sie sich direkt bei uns im Laden an oder kontaktieren Sie uns bei Fragen telefonisch oder per Mail.
+                        </p>
+                      </div>
+                    </li>
+
+                    <li>
+                      <div class="collapsible-header"><i class="material-icons">group</i>Stricktreff am Dienstag (21.02.)</div>
+                        <div class="collapsible-body"><p>
+                        Stricken alleine langweilt euch? Dann kommt doch zu uns in die Garntante. <br>
+                        Bei einer Tasse Kaffe treffen wir uns jeden <b>1. und 3. Dienstag</b> im Monat von 17:00 bis 19:00 Uhr um in lockerer Runde gemeinsam die
+                        Nadeln klappern zu lassen.<br><br>
+                        <b>Dauer: </b>17:00 - 19:00 Uhr<br>
+                        <b>Termin: </b>21.02.2017<br>
+                        </p>
+                      </div>
+                    </li>
+
+                    <li>
+                      <div class="collapsible-header"><i class="material-icons">event_note</i>Raglan von Oben (17.02. + 03.03.)</div>
+                        <div class="collapsible-body"><p>
+                        In diesem Kurs erlernen Sie, wie Sie einen Raglanpullover stricken.<br><br>
+                        <b>Dauer: </b>5 Stunden (3 Stunden am ersten, 2 Stunden am zweiten Termin)<br>
+                        <b>Termine: </b>17.02. von 16 bis 19 Uhr und am 03.03. von 16 bis 18 Uhr<br>
+                        <b>Teilnahmegeb&uuml;hr: </b>40€ pro Person<br><br>
+                        Bitte melden Sie sich direkt bei uns im Laden an oder kontaktieren Sie uns bei Fragen telefonisch oder per Mail.
+                        </p>
+                      </div>
+                    </li>
                   </ul>
 
                   <h4 class="center">März '17</h4>
                   <ul class="collapsible popout" data-collapsible="accordion">
                     <li>
-                      <div class="collapsible-header"><i class="material-icons">group</i>Stricktreff am Dienstag</div>
+                      <div class="collapsible-header"><i class="material-icons">group</i>Stricktreff am Dienstag (07.03.)</div>
                       <div class="collapsible-body"><p>
                       Stricken alleine langweilt euch? Dann kommt doch zu uns in die Garntante. <br>
                       Bei einer Tasse Kaffe treffen wir uns jeden <b>1. und 3. Dienstag</b> im Monat von 17:00 bis 19:00 Uhr um in lockerer Runde gemeinsam die
                       Nadeln klappern zu lassen.<br><br>
                       <b>Dauer: </b>17:00 - 19:00 Uhr<br>
-                      <b>Termin: </b>' . $datum . '<br>
+                      <b>Termin: </b>07.03.<br>
                       </p>
                     </div>
                   </li>
                     <li>
-                      <div class="collapsible-header"><i class="material-icons">group</i>Stricktreff am Dienstag</div>
+                      <div class="collapsible-header"><i class="material-icons">event_note</i>Toe up Sockenstrickkurs (10.03. + 17.03.)</div>
+                      <div class="collapsible-body"><p>
+                      Wenn Sie bereits rechte und linke Maschen stricken können und sich nun an das Projekt Socke wagen möchten,
+                      sind Sie bei uns genau richtig. Angefangen an der Spitze stricken wir gemeinsam hinüber zur Bumerang Ferse. <br><br>
+                      <b>Dauer: </b>4 Stunden (jeweils 2 Stunden pro Termin)<br>
+                      <b>Termine: </b>10.03. + 17.03. von 17 bis 19 Uhr<br>
+                      <b>Teilnahmegeb&uuml;hr: </b>40€ pro Person<br>
+                      <b>Material: </b>
+                      Benötigt werden 100g 4-Fach Sockengarn (LL ca 420m / 100g) sowie ein Nadelspiel in Stärke 2,5 bis 3 mm.
+                      Das Material kann vor Ort erworben oder von zu Hause mitgebracht werden.
+
+                      <br><br>
+                      Bitte melden Sie sich direkt bei uns im Laden an oder kontaktieren Sie uns bei Fragen telefonisch oder per Mail.
+                      </p>
+                    </div>
+                  </li>
+                    <li>
+                      <div class="collapsible-header"><i class="material-icons">group</i>Stricktreff am Dienstag (21.03.)</div>
                       <div class="collapsible-body"><p>
                       Stricken alleine langweilt euch? Dann kommt doch zu uns in die Garntante. <br>
                       Bei einer Tasse Kaffe treffen wir uns jeden <b>1. und 3. Dienstag</b> im Monat von 17:00 bis 19:00 Uhr um in lockerer Runde gemeinsam die
                       Nadeln klappern zu lassen.<br><br>
                       <b>Dauer: </b>17:00 - 19:00 Uhr<br>
-                      <b>Termin: </b>' . $datum . '<br>
+                      <b>Termin: </b>21.03.<br>
                       </p>
                     </div>
                   </li>
